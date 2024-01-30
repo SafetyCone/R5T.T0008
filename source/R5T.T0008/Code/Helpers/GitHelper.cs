0e44eb9f1058a62698db7082d8631134ef8a6a2f
davidcoats <david@getsafetycone.com>
2024-01-29 18:34:35 -0800

    Interim changes.

--- source/R5T.T0008/Code/Helpers/GitHelper.cs
+++ source/R5T.T0008/Code/Helpers/GitHelper.cs
@@ -3,6 +3,12 @@
 
 namespace R5T.T0008
 {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// See new work in: R5T.L0083.F001.
+    /// </remarks>
     public static class GitHelper
     {
         public const string OriginRemoteName = "origin";
